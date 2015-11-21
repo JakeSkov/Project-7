c036d94a77e369af886c2a1872f1fedda1b05a7f
LivingValkyrie <deadwynn@gmail.com>
2015-11-21 11:35:42 -0600

    start pick up system
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ UnityProject/Assets/PickUp.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: PickUp 
+/// </summary>
+public class PickUp : MonoBehaviour {
+    #region Fields
+
+    public Transform pickUpTransform;
+    bool hasFlag = false;
+    int score = 0;
+    #endregion
+
+    void Start() { }
+
+    void Update() { }
+
+    void OnTriggerEnter(Collider other) {
+
+        switch (other.tag) {
+            case "Flag":
+                other.transform.parent = this.transform;
+                other.transform.position = pickUpTransform.position;
+                hasFlag = true;
+                break;
+            case "PickUpObject":
+                print("picked up " + other.name);
+                break;
+            case "FlagDrop":
+                hasFlag = false;
+                score++;
+                break;
+        }
+    }
+
+}
